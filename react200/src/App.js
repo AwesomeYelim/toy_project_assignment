a0011f3b4d578cd91dcb343457a93de79b2c4e41
ylhong <ylhong@ssrinc.co.kr>
2022-07-05 15:41:02 +0900

    '17'

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './App.css';
-import ArrowFunc from './R013_ArrowFunction';
+import Props from './R017_Props';
 
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
-      <p>HTML 적용하기</p>
-      <ArrowFunc />
+      <p>CSS 적용하기</p>
+      <Props props_val='THIS IS PROPS' />
     </div>
   );
 }
