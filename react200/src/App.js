c45a56a7e17052a4f3cdb2fe9b514601cd86eec2
Awesomeyelim <uiop01900@gmail.com>
2022-07-12 08:03:25 +0900

    018

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import './App.css';
-import Map from "./R015_Map"
-
+import React from "react";
+import "./App.css";
+import PropsDataType from "./R018_PropsDatatype";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Map />
+      <PropsDataType
+        String="react"
+        Number={200}
+        Boolean={1 == 1}
+        Array={[0, 1, 8]}
+        Object
+        Json={{ react: "리액트", twohunred: "200" }}
+        Function={console.log("FunctionProps: function!")}
+      />
     </div>
   );
 }
