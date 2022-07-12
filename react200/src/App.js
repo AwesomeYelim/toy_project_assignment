eec57c04af15235b46805fa1ea796929eeda68b5
Awesomeyelim <uiop01900@gmail.com>
2022-07-13 08:21:20 +0900

    019

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,21 +1,14 @@
 import React from "react";
 import "./App.css";
-import PropsDataType from "./R018_PropsDatatype";
+import PropsBoolean from "./R019_PropsBoolean";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsDataType
-        String="react"
-        Number={200}
-        Boolean={1 == 1}
-        Array={[0, 1, 8]}
-        Object
-        Json={{ react: "리액트", twohunred: "200" }}
-        Function={console.log("FunctionProps: function!")}
-      />
+      <PropsBoolean BooleanTrueFalse={false}/>
+      <PropsBoolean BooleanTrueFalse/>
     </div>
   );
 }
