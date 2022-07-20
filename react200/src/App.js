18f602b55695f13e4e7f10520d940e33adba63c9
Awesomeyelim <uiop01900@gmail.com>
2022-07-21 08:26:44 +0900

    023

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import PropsDefalut from "./R022_PropsDefalut";
+import PropsNode from "./R023_PropsNode";
 
 function App() {
   return (
     <div>
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <PropsDefalut ReactNumber={200} />
+      <PropsNode>
+        <span>node from App.js</span>
+      </PropsNode>
     </div>
   );
 }
