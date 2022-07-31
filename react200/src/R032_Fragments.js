347df12afb597d9351cdad4a5549b59bd0c019c4
Awesomeyelim <uiop01900@gmail.com>
2022-08-01 08:33:06 +0900

    032

---
+++ react200/src/R032_Fragments.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class R032_Fragments extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>P TAG</p>
+        <span>SPAN TAG</span>
+      </React.Fragment>
+    );
+  }
+}
+
+export default R032_Fragments;
