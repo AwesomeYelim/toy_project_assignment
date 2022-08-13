15fe926e3de031f7af38f1824706927264e250bb
Awesomeyelim <uiop01900@gmail.com>
2022-08-13 17:53:33 +0900

    066

--- react200/src/R066_onClick.js
+++ react200/src/R066_onClick.js
@@ -6,7 +6,7 @@ class R066_onClick extends Component {
   
     buttonClick = (param) => {
       if(typeof param != 'string') param = 'Click a'
-      console.log("parma" + param);
+      console.log("parma : " + param);
     }
 
   render() {
@@ -18,7 +18,7 @@ class R066_onClick extends Component {
     <div onClick={e => this.buttonClick("Click div")}>
       Click div
     </div>
-    <a href="javascript: " onClick={this.buttonClick}>Click a</a>
+    <a href="javascript: " onClick={this.buttonClick('click a')}>Click a</a>
     </>
     );
   }
