d6d286f801afd3a3efc6003a45285afd1b65cfce
Awesomeyelim <uiop01900@gmail.com>
2022-08-27 15:21:08 +0900

    080

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
-import StrAddButton from "./StrAddButton"
+import React, { Component } from "react";
+import StrAddButton from "./StrAddButton";
 
-function App() {
-  return (
-    <div>
-      <h1>Start React 200!</h1>
-      <span>{this.props.store.getState().data.str}</span><br/>
-      <StrAddButton store={this.props.store}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Start React 200!</h1>
+        <span>{this.props.store.getState().data.str}</span>
+        <br />
+        <StrAddButton store={this.props.store} />
+      </div>
+    );
+  }
 }
- 
+
 export default App;
--- react200/src/index.js
+++ react200/src/index.js
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import './index.css'
-import App from './App';
-import {configureStore} from 'redux';
-import reducers from './reducers';
+import "./index.css";
+import App from "./App";
+import { createStore } from "redux";
+import reducers from "./reducers";
 
-
-const store = configureStore(reducers);
+const store = createStore(reducers);
 
 const listener = () => {
-  ReactDOM.render(
-    <App store={store} />,
-    document.getElementById('root')
-  ) 
-}
+  ReactDOM.render(<App store={store} />, document.getElementById("root"));
+};
 
 store.subscribe(listener);
 listener();
-
-
