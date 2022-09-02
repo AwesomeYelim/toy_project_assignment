375140702aae742fc4be14affcee3c822666be4a
Awesomeyelim <uiop01900@gmail.com>
2022-09-02 15:06:21 +0900

    05

--- react200/src/App.js
+++ react200/src/App.js
@@ -1,29 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import StrAddButton from "./StrAddButton";
+import React from "react";
+import CookieSave from "./R085_cookieSave";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Start React 200!</h1>
-        {/* <span>{this.props.store.getState().data.str}</span>
-        <br /> */}
-        <span>{this.props.str}</span><br/>
-        {/* <StrAddButton store={this.props.store} /> */}
-        <StrAddButton AppProp='200'/>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Start React 200!</h1>
+      <CookieSave />
+    </div>
+  );
 }
-
-let mapStateToProps = (state, props) =>{
-  console.log('Props from index.js : ' + props.indexProps)
-  return {
-    str: state.data.str,
-  }
-}
-
-App = connect(mapStateToProps, null)(App)
 
 export default App;
--- react200/src/index.js
+++ react200/src/index.js
@@ -1,42 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { createStore, applyMiddleware } from "redux";
-import { Provider } from "react-redux";
-import reducers from "./reducers";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const CallMiddleware = (store) => (nextMiddle) => (action) => {
-  console.log("1. reducer 실행 전");
-  console.log(
-    "2. action.type : " +
-      action.type +
-      ", store str :" +
-      store.getState().data.str
-  );
-  let result = nextMiddle(action);
-  console.log("3. reducer 실행 후");
-  console.log(
-    "4. action.type : " +
-      action.type +
-      ", store str :" +
-      store.getState().data.str
-  );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
-  return result;
-};
-
-const store = createStore(reducers, applyMiddleware(CallMiddleware));
-
-const listener = () => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <App indexProps="react" />
-    </Provider>,
-    document.getElementById("root")
-  );
-};
-
-store.subscribe(listener);
-listener();
-
+reportWebVitals();
